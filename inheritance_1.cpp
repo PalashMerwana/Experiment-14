a313e4fdc3e28072544be271185195dc0fec8cfe
PalashMerwana <palash.merwana.btech2023@sitpune.edu.in>
2024-09-15 08:55:50 +0530

    Add files via upload

---
+++ inheritance_1.cpp
@@ -0,0 +1,31 @@
+// Palash Merwana 
+// 23070123095
+// experiment 13: inheritance
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class uni
+{
+    public:
+    string uni = "symbiosis: ";
+    void discipline()
+    {
+        cout<<"engineering "<<endl;
+    }
+};
+
+class dep: public uni
+{
+    public:
+    string dept="electronics and telecommunication ";
+};
+
+int main()
+{
+    dep u1;
+    u1.discipline();
+    cout<<u1.uni+" "+u1.dept;
+}
